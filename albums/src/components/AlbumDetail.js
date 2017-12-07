42c15b02631f941b75789e1a9526caf3e690fdf0
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2017-12-07 18:53:05 +0200

    added styles

--- albums/src/components/AlbumDetail.js
+++ albums/src/components/AlbumDetail.js
@@ -1,10 +1,43 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, Image } from 'react-native';
+import Card from './Card';
+import CardSection from './CardSection';
 
-const AlbumDetail = album => (
-    <View>
-      <Text>{album.title}</Text>
-    </View>
-  );
+const AlbumDetail = ({ album }) => (
+    <Card>
+      <CardSection>
+        <View style={styles.thumbnailContainerStyle}>
+          <Image
+            source={{ uri: album.thumbnail_image }}
+            style={styles.thumbnailStyle}
+          />
+        </View>
+        <View style={styles.headerContentStyle}>
+          <Text style={styles.headerTextStyle}>{album.title}</Text>
+          <Text>{album.artist}</Text>
+        </View>
+      </CardSection>
+    </Card>
+);
+
+const styles = {
+  headerContentStyle: {
+    flexDirection: 'column',
+    justifyContent: 'space-around',
+  },
+  headerTextStyle: {
+    fontSize: 18
+  },
+  thumbnailStyle: {
+    height: 50,
+    width: 50
+  },
+  thumbnailContainerStyle: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+    marginRight: 10,
+  }
+};
 
 export default AlbumDetail;
--- albums/src/components/Card.js
+++ albums/src/components/Card.js
@@ -1,6 +1,28 @@
 import React from 'react';
 import { View } from 'react-native';
 
-const Card = () => <View />;
+const Card = props => (
+  <View style={styles.containerStyle}>
+    {props.children}
+  </View>
+);
+
+const styles = {
+  containerStyle: {
+    borderWidth: 1,
+    borderRadius: 2,
+    borderColor: '#DDD',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 1,
+    marginBottom: 10,
+  }
+};
 
 export default Card;
