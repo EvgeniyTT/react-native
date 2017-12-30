43c3d2f8d9e26d266806d0cbf8d3e31c453b93bd
Evgeniy Tatarin <tatarin.e.m@gmail.com>
2017-12-30 14:02:14 +0200

    updated app and login form

--- auth/src/App.js
+++ auth/src/App.js
@@ -1,45 +1,51 @@
 import React, { Component } from 'react';
-import { Text, Platform, View, StyleSheet } from 'react-native';
+import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
 
-  initFirebase = () => {
-    console.log('BABABAB')
-    console.log('firebase.apps.length: ', firebase.apps.length)
-    if (!firebase.apps.length) {
-      firebase.initializeApp({
-        apiKey: "AIzaSyBvRVflt3bq6qnXaddTZRuUCOCGlBnRGVo",
-        authDomain: "albums-auth-6437a.firebaseapp.com",
-        databaseURL: "https://albums-auth-6437a.firebaseio.com",
-        projectId: "albums-auth-6437a",
-        storageBucket: "albums-auth-6437a.appspot.com",
-        messagingSenderId: "759591123631"
-      });
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: "AIzaSyBvRVflt3bq6qnXaddTZRuUCOCGlBnRGVo",
+      authDomain: "albums-auth-6437a.firebaseapp.com",
+      databaseURL: "https://albums-auth-6437a.firebaseio.com",
+      projectId: "albums-auth-6437a",
+      storageBucket: "albums-auth-6437a.appspot.com",
+      messagingSenderId: "759591123631"
+    });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent() {
+    switch (this.state.loggedIn) {
+      case true:
+        return (
+          <Button onPress={() => firebase.auth().signOut()}>
+            Log Out
+          </Button>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />;
     }
   }
 
-  componentWillMount() {
-    this.initFirebase();
-    firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
-        console.log('user: ', user);
-      }
-    })
-  }
-
-  componentWillReceiveProps() {
-    console.log('RECEIVE')
-  }
-
   render() {
-    this.initFirebase();
     return (
       <View>
-        <Header headerText='Auth' />
-        <LoginForm initFirebase={this.initFirebase}/>
+        <Header headerText="Authentication" />
+        {this.renderContent()}
       </View>
     );
   }
--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -4,75 +4,87 @@ import firebase from 'firebase';
 import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { email: '', password: '', error: {}, loading: false };
-  }
-  
-  onButtonPressed = () => {
-    this.props.initFirebase();
+  state = { email: '', password: '', error: '', loading: false };
+
+  onButtonPress() {
     const { email, password } = this.state;
 
-    // fetch('https://www.youtube.com/watch?v=lJAYI8IiL2g');
+    this.setState({ error: '', loading: true });
 
-    this.setState({ error: {}, loading: true })
     firebase.auth().signInWithEmailAndPassword(email, password)
-      .then(this.onLoginSuccess)
-      .catch(err => {
-        console.log('SEWCONF')
+      .then(this.onLoginSuccess.bind(this))
+      .catch(() => {
         firebase.auth().createUserWithEmailAndPassword(email, password)
-            .then(this.onLoginSuccess)
-            .catch(this.onLoginFail)
-      })
-      .then(res => console.log('FINALLY'))
+          .then(this.onLoginSuccess.bind(this))
+          .catch(this.onLoginFail.bind(this));
+      });
   }
 
-  onLoginSuccess = () => {
-    console.log('LOGIN SUCCESS!')
-    this.setState({ email: '', password: '', error: {}, loading: false });
+  onLoginFail() {
+    this.setState({ error: 'Authentication Failed', loading: false });
   }
 
-  onLoginFail = error => {
-    console.log('LOGIN FAILED!')
-    this.setState({ error, loading: false });
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    });
   }
 
-  renderButton = () => this.state.loading
-    ? <Spinner />
-    : <Button onPress={this.onButtonPressed}>Log in</Button>
+  renderButton() {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress.bind(this)}>
+        Log in
+      </Button>
+    );
+  }
 
   render() {
     return (
       <Card>
         <CardSection>
           <Input
-            label="Email"
-            onChangeText={email => { this.setState({ email }) }}
             placeholder="user@gmail.com"
+            label="Email"
             value={this.state.email}
+            onChangeText={email => this.setState({ email })}
           />
         </CardSection>
 
         <CardSection>
           <Input
-            label="Password"
-            onChangeText={password => { this.setState({ password }) }}
-            placeholder="password"
             secureTextEntry
+            placeholder="password"
+            label="Password"
             value={this.state.password}
+            onChangeText={password => this.setState({ password })}
           />
         </CardSection>
 
-        <CardSection>
-          <Text>{this.state.error.message}</Text>
-        </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
 
         <CardSection>
           {this.renderButton()}
         </CardSection>
       </Card>
-    )
+    );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
